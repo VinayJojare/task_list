710fabab6935c874d463e4a9c872ba1280f4b9c9
VinayJojare <jojarevinay10@gmail.com>
2024-04-05 00:52:33 +0530

    vercel update done

--- class_based_view/settings.py
+++ class_based_view/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k_h668_bl6vhytsw12#=q%qs@3@&rz_l!a9#m!ma(t)_%_we&q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+import os 
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
